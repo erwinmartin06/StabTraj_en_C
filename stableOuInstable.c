f5d4ca62065a08217ba1da20bd4e0aef2f5d6c94
Erwin MARTIN <erwin.martin@etu.emse.fr>
2024-01-13 16:33:08 +0100

    Second Commit

--- stableOuInstable.c
+++ stableOuInstable.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Function Prototypes
 double calculateDistance(double Cn, double Cn0, double MS_min, double MS_max, double MS_Cn_min, double MS_Cn_max, double CritCnmin, double CritCnmax, double CritMsmin, double CritMsmax, double CritMsCnmin, double CritMsCnmax);
@@ -50,15 +52,21 @@ int main(int argc, char *argv[]) {
     int Q_can = atoi(argv[10]);
     int X_ail = atoi(argv[11]);
     int X_can = atoi(argv[12]);
+    int D2j = atoi(argv[13]);
 
     // Formulas
     int D_ail = D_ref;
     int D_can = D_ref;
+    int D1j = D_og;
+    int D2r = D_og;
+    int D1r = D2j;
 
     // Variable declarations that will stock the best values found
     double best_m_ail = 0, best_n_ail = 0, best_p_ail = 0, best_E_ail = 0;
     double best_m_can = 0, best_n_can = 0, best_p_can = 0, best_E_can = 0;
     double minDistanceGlobal = DBL_MAX;
+    double Q_int = (Q_ail == Q_can) ? Q_ail : 0;
+    double D_int = D_ail;
 
     // Critical values
     double CritCnmin = calculateCritCnmin(Type_fusee);
@@ -77,17 +85,33 @@ int main(int argc, char *argv[]) {
                         for (double m_can = 50; m_can <= 300; m_can += currentStep) {
                             for (double n_can = 50; n_can <= m_can; n_can += currentStep) {
                                 for (double p_can = 0; p_can <= n_can; p_can += currentStep) {
+                                    // Calculations
+                                    double E_int;
+                                    if ((D_can / 2.0 + E_can) <= (D_ail / 2.0)) {
+                                        E_int = 0;
+                                    } else if ((D_can / 2.0 + E_can) >= (D_ail / 2.0 + E_ail)) {
+                                        E_int = E_ail;
+                                    } else {
+                                        E_int = (D_can / 2.0 + E_can) - (D_ail / 2.0);
+                                    }
+
+                                    double m_int = m_ail;
+                                    double n_int = n_ail + (m_ail-n_ail) * (1 - E_int/E_ail);
+                                    double p_int = p_ail * E_int/E_ail;
+
+                                    double f_int = sqrt(pow(p_int + n_int / 2.0 - m_int / 2.0, 2) + pow(E_int, 2));
+
                                     // Calculations of values "Portance", "MS", and "Couple" for the entire rocket
-                                    double Cn = calculateCn(Type_masquage_bi, Q_ail, E_ail, D_ref, D_ail, X_ail, m_ail, n_ail, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-                                    double Cn0 = calculateCn0(Type_masquage_bi, Q_ail, E_ail, D_ref, D_ail, X_ail, m_ail, n_ail, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, 0, 0, 0, 0);
+                                    double Cn = calculateCn(Type_masquage_bi, Q_ail, E_ail, D_ref, D_ail, X_ail, m_ail, n_ail, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, D2j, D1j, D2r, D1r, Q_int, E_int, D_int, f_int, m_int, n_int);
+                                    double Cn0 = calculateCn0(Type_masquage_bi, Q_ail, E_ail, D_ref, D_ail, X_ail, m_ail, n_ail, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, D2j, D1j, D2r, D1r);
                                     double MS_min = calculateMS_min(X_ail, XpropuRef, D_ref);
                                     double MS_max = calculateMS_max(X_can, XpropuRef, D_ref);
                                     double MS_Cn_min = calculateMS_Cn_min(MS_min, Cn);
                                     double MS_Cn_max = calculateMS_Cn_max(MS_max, Cn0);
 
                                     // Calculations of values "Portance", "MS", and "Couple" for the ship
-                                    double CnShip = calculateCn(Type_masquage_ship, Q_ail, E_ail, D_ref, D_ail, X_ail, m_ail, n_ail, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-                                    double Cn0Ship = calculateCn0(Type_masquage_ship, Q_ail, E_ail, D_ref, D_ail, X_ail, m_ail, n_ail, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, 0, 0, 0, 0);
+                                    double CnShip = calculateCn(Type_masquage_ship, Q_ail, E_can, D_ref, D_ail, X_ail, m_can, n_can, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, D2j, D1j, D2r, D1r, Q_int, E_int, D_int, f_int, m_int, n_int);
+                                    double Cn0Ship = calculateCn0(Type_masquage_ship, Q_ail, E_can, D_ref, D_ail, X_ail, m_can, n_can, Q_can, E_can, D_can, X_can, m_can, n_can, D_og, D2j, D1j, D2r, D1r);
                                     double MS_minShip = calculateMS_min(X_ail, XpropuRef, D_ref);
                                     double MS_maxShip = calculateMS_max(X_can, XpropuRef, D_ref);
                                     double MS_Cn_minShip = calculateMS_Cn_min(MS_minShip, CnShip);
@@ -129,7 +153,6 @@ int main(int argc, char *argv[]) {
 
 //Calculation of values "Portance", "MS", and "Couple"
 double calculateCn(const char* Type_masquage, double Q_ail, double E_ail, double D_ref, double D_ail, double f_ail, double m_ail, double n_ail, double Q_can, double E_can, double D_can, double f_can, double m_can, double n_can, double D_og, double D2j, double D1j, double D2r, double D1r, double Q_int, double E_int, double D_int, double f_int, double m_int, double n_int) {
-    
     // Cni calculation
     double Cni = 0;
     if (Type_masquage[0] == 'B') {
@@ -162,7 +185,6 @@ double calculateCn(const char* Type_masquage, double Q_ail, double E_ail, double
 }
 
 double calculateCn0(const char* Type_masquage, double Q_ail, double E_ail, double D_ref, double D_ail, double f_ail, double m_ail, double n_ail, double Q_can, double E_can, double D_can, double f_can, double m_can, double n_can, double D_og, double D2j, double D1j, double D2r, double D1r) {
-    
     // Cnail calculation
     double Cnail = 4 * Q_ail * pow(E_ail / D_ref, 2) * (1 + D_ail / (2 * E_ail + D_ail)) / (1 + sqrt(1 + pow(2 * f_ail / (m_ail + n_ail), 2)));
 
